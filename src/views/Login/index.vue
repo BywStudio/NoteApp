a9b78a99dacc7ac99ee8de6e7a9076cecd37e32e
byw <19941829077@163.com>
2025-03-31 18:34:49 +0800

    初始化一级路由配置

---
+++ src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Login</h1>
+    </header>
+    <main>
+      
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  color: #fff;
+}
+</style>
